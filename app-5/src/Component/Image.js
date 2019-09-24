c77753bed6c7f7651995509f47865b1fcad49e08
unknown <malloryjmathews@gmail.com>
2019-09-24 15:09:13 -0600

    app-5

---
+++ app-5/src/Component/Image.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Image extends Component {
+    render() {
+        return (
+            <div>
+                <img src='https://media.comicbook.com/2019/09/animatronic-it-pennywise-halloween-prop-1186448.jpeg'/>
+            </div>
+        )
+    }
+}
